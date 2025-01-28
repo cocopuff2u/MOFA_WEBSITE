69a3dd7a067caa6a9cb70096c49584225edcd2fa
Cody Keats <cocopuff2u@yahoo.com>
2025-01-28 17:52:08 -0500

    Changing Automation to run every 2 hours

--- update_readme_scripts/update_ios_appstore_current_version_readme.py
+++ update_readme_scripts/update_ios_appstore_current_version_readme.py
@@ -136,7 +136,7 @@ lastUpdated: false
 # <img src="/images/App_Store_logo.png" alt="image" width="40" style="vertical-align: middle; display: inline-block;" /> iOS App Store Latest Updates
 
 <span class="extra-small">_Last Updated: <code style="color : dodgerblue">{last_updated}</code> [**_Raw XML_**](https://github.com/cocopuff2u/MOFA/blob/main/latest_raw_files/ios_appstore_latest.xml) [**_Raw YAML_**](https://github.com/cocopuff2u/MOFA/blob/main/latest_raw_files/ios_appstore_latest.yaml) [**_Raw JSON_**](https://github.com/cocopuff2u/MOFA/blob/main/latest_raw_files/ios_appstore_latest.json)
- (Automatically Updated every 4 hours)_</span>
+ (Automatically Updated every 2 hours)_</span>
 
 | Application Name | Version | Bundle ID | Minimum OS Version | Icon |
 |------------------|---------|-----------|-------------------|------|
