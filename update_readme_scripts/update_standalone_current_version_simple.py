c8bb80dd204e25194f9fbf06e91a77e0058e134a
cocopuff2u <cocopuff2u@yahoo.com>
2025-03-27 09:33:05 -0400

    Aligning Center

--- update_readme_scripts/update_standalone_current_version_simple.py
+++ update_readme_scripts/update_standalone_current_version_simple.py
@@ -69,9 +69,9 @@ This page is currently under construction.
 Stay tuned for updates!
 :::
 
-</div>
-
 <span class="extra-small">_Last Updated: <code style="color : dodgerblue">{global_last_updated}</code> [**_Raw XML_**](https://github.com/cocopuff2u/MOFA/blob/main/latest_raw_files/macos_standalone_latest.xml) [**_Raw YAML_**](https://github.com/cocopuff2u/MOFA/blob/main/latest_raw_files/macos_standalone_latest.yaml) [**_Raw JSON_**](https://github.com/cocopuff2u/MOFA/blob/main/latest_raw_files/macos_standalone_latest.json) (Automatically Updated every 2 hours)_</span>
+
+</div>
 
 <div style="display: flex; justify-content: center;">
 
